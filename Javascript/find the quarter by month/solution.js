b518d7fdef444434220041d9fe224d1d33147d7b
Drew <dreww.dev@gmail.com>
2022-08-02 19:45:03 -0400

    find the quarter by month

---
+++ Javascript/find the quarter by month/solution.js
@@ -0,0 +1,13 @@
+// problem: https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/javascript
+
+const quarterOf = (month) => {
+  if (month <= 3) {
+    return 1;
+  } else if (month <= 6) {
+    return 2;
+  } else if (month <= 9) {
+    return 3;
+  } else {
+    return 4;
+  }
+};
