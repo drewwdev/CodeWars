f1ba3078c7d5f78ea4dfdda10fb98ae6a3ec767d
Drew <dreww.dev@gmail.com>
2022-08-06 10:21:43 -0400

    difference of cuboids

---
+++ Javascript/difference of volumes of cuboids/solution.js
@@ -0,0 +1,7 @@
+// problem: https://www.codewars.com/kata/58cb43f4256836ed95000f97/train/javascript
+
+function findDifference(a, b) {
+  return Math.abs(
+    a.reduce((x, y) => x * y) - (bTotal = b.reduce((x, y) => x * y))
+  );
+}
