b32ed078f9b4ce8f8425946dff51d45aea22d5f2
Drew <dreww.dev@gmail.com>
2022-08-06 09:47:57 -0400

    solution for finding if a string ends with a string

---
+++ Javascript/solution.js
@@ -0,0 +1,12 @@
+problem: //www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/javascript
+
+https: function solution(str, ending) {
+  const endingLength = ending.length;
+  const numberToTrim = str.length - endingLength;
+  const trimmedString = str.substring(numberToTrim, 0);
+  if (trimmedString.concat(ending) === str) {
+    return true;
+  } else {
+    return false;
+  }
+}
